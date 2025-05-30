da0bae7c3d6e16dcddd8f736dfc1ab79a1132221
Pedro Barros <pedrobars7a@gmail.com>
2025-05-29 22:19:24 -0300

    feat: instal shadcn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div className="min-h-screen min-w-full bg-slate-500 flex items-center justify-center">
+    <div className="min-h-screen min-w-full flex items-center justify-center">
       <h2 className="text-4xl font-bold text-red-500">Title</h2>
+      <Button variant="outline">Click me</Button>
     </div>
   );
 }
