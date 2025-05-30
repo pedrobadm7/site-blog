84508cf66f19c95036d11b36482b963d4efd4ae1
Pedro Barros <pedrobars7a@gmail.com>
2025-05-29 22:12:42 -0300

    first commit

---
+++ src/pages/blog/posts/[...slug].tsx
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+
+export default function Post() {
+  const router = useRouter()
+  const segments = router.query.slug as string[]
+
+  return <div>Post {segments?.join("/")}</div>;
+}
