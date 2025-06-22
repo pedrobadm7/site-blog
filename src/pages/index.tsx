662098a51fbad18935197a63016f1fe87958f22c
Pedro Barros <pedrobars7a@gmail.com>
2025-06-22 18:16:55 -0300

    ffeat: setup contentlayer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { allPosts } from 'contentlayer/generated'
+
 import { LandingPage } from '@/templates/landing-page'
 
 export default function Home() {
+  const posts = allPosts
+
+  console.log(posts)
+
   return <LandingPage />
 }
